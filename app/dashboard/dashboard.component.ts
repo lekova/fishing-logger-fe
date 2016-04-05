b5555453028c15c78768e3712e3c2ddb91a1a3dd
Lina Lekova <lina@netagesolutions.com>
2016-04-05 16:48:21 -0400

    Add Dashboard component scaffold

---
+++ app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnDestroy, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
+import { Observable, Subscription } from 'rxjs/Rx';
+
+@Component({
+    selector: 'my-dashboard',
+    templateUrl: 'app/dashboard/dashboard.component.html',
+    styleUrls: ['app/dashboard/dashboard.component.css']
+})
+export class DahboardComponent {
+
+}
