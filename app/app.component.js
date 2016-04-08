0e3a25c7e8c000d881a5ac3fb15833832babecb0
Lina Lekova <lina@LinaPC2016.netagesolutions.com>
2016-04-08 13:51:10 -0400

    Add inital code to app.component

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/http', 'angular2/router', 'rxjs/Rx', './fishinglogs/fishinglogs', './dashboard/dashboard.component', './shared/shared', './blocks/blocks'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,25 +10,77 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, http_1, router_1, fishinglogs_1, dashboard_component_1, shared_1, blocks_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (_1) {},
+            function (fishinglogs_1_1) {
+                fishinglogs_1 = fishinglogs_1_1;
+            },
+            function (dashboard_component_1_1) {
+                dashboard_component_1 = dashboard_component_1_1;
+            },
+            function (shared_1_1) {
+                shared_1 = shared_1_1;
+            },
+            function (blocks_1_1) {
+                blocks_1 = blocks_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
-                    this.title = 'Fishing Logger';
+                function AppComponent(_messageService, _modalService) {
+                    this._messageService = _messageService;
+                    this._modalService = _modalService;
+                    this.menuItems = [
+                        { caption: 'Dashboard', link: ['Dashboard'] },
+                        { caption: 'Fishing Logs', link: ['Fishinglogs'] }
+                    ];
                 }
+                AppComponent.prototype.resetDb = function () {
+                    var _this = this;
+                    var msg = 'Are you sure you want to reset the database?';
+                    this._modalService.activate(msg).then(function (responseOK) {
+                        if (responseOK) {
+                            _this._messageService.resetDb();
+                        }
+                    });
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'fishing-logger-app',
                         templateUrl: 'app/app.component.html',
-                        styleUrls: ['app/app.component.css']
-                    }), 
-                    __metadata('design:paramtypes', [])
+                        styleUrls: ['app/app.component.css'],
+                        directives: [router_1.ROUTER_DIRECTIVES, blocks_1.ModalComponent, blocks_1.SpinnerComponent, blocks_1.ToastComponent],
+                        providers: [
+                            http_1.HTTP_PROVIDERS,
+                            // provide(XHRBackend, { useClass: InMemoryBackendService }),
+                            // provide(SEED_DATA, { useClass: InMemoryStoryService }),
+                            // provide(InMemoryBackendConfig, { useValue: { delay: 600 } }),
+                            router_1.ROUTER_PROVIDERS,
+                            fishinglogs_1.FishinglogService,
+                            blocks_1.EntityService,
+                            blocks_1.ExceptionService,
+                            shared_1.MessageService,
+                            blocks_1.ModalService,
+                            blocks_1.SpinnerService,
+                            blocks_1.ToastService
+                        ]
+                    }),
+                    router_1.RouteConfig([
+                        { path: '/dashboard', name: 'Dashboard', component: dashboard_component_1.DashboardComponent, useAsDefault: true },
+                        { path: '/fishinglogs/...', name: 'Fishinglogs', component: fishinglogs_1.FishinglogsComponent },
+                    ]), 
+                    __metadata('design:paramtypes', [shared_1.MessageService, blocks_1.ModalService])
                 ], AppComponent);
                 return AppComponent;
             }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,57 @@
 import { Component } from 'angular2/core';
+import { HTTP_PROVIDERS, XHRBackend } from 'angular2/http';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import 'rxjs/Rx'; // load the full rxjs
+
+// import { InMemoryBackendConfig, InMemoryBackendService, SEED_DATA } from 'a2-in-memory-web-api/core';
+// import { InMemoryStoryService } from '../api/in-memory-story.service';
+import { FishinglogsComponent, FishinglogService } from './fishinglogs/fishinglogs';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessageService } from './shared/shared';
+import { EntityService, ExceptionService, ModalComponent, ModalService,
+         SpinnerComponent, SpinnerService, ToastComponent, ToastService } from './blocks/blocks';
 
 @Component({
   selector: 'fishing-logger-app',
   templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES, ModalComponent, SpinnerComponent, ToastComponent],
+  providers: [
+    HTTP_PROVIDERS,
+    // provide(XHRBackend, { useClass: InMemoryBackendService }),
+    // provide(SEED_DATA, { useClass: InMemoryStoryService }),
+    // provide(InMemoryBackendConfig, { useValue: { delay: 600 } }),
+    ROUTER_PROVIDERS,
+    FishinglogService,
+    EntityService,
+    ExceptionService,
+    MessageService,
+    ModalService,
+    SpinnerService,
+    ToastService
+  ]
 })
+@RouteConfig([
+  { path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true },
+  { path: '/fishinglogs/...', name: 'Fishinglogs', component: FishinglogsComponent },
+])
 export class AppComponent {
-  title = 'Fishing Logger';
+  public menuItems = [
+    { caption: 'Dashboard', link: ['Dashboard'] },
+    { caption: 'Fishing Logs', link: ['Fishinglogs'] }
+  ];
+
+  constructor(
+    private _messageService: MessageService,
+    private _modalService: ModalService) {
+  }
+
+  resetDb() {
+    let msg = 'Are you sure you want to reset the database?';
+    this._modalService.activate(msg).then(responseOK => {
+      if (responseOK) {
+        this._messageService.resetDb();
+      }
+    });
+  }
 }
