3494549c8502741f0e41a9de8e169da70da3a7bf
lekova <lina.lekova@gmail.com>
2016-04-11 07:58:18 -0400

    Add app-injector and components to main.ts

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
-System.register(['angular2/platform/browser', './app.component'], function(exports_1, context_1) {
+System.register(['angular2/platform/browser', './app.component', 'angular2/http', 'angular2/core', 'angular2/router', './auth/auth.service', './app-injector'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, app_component_1;
+    var browser_1, app_component_1, http_1, core_1, router_1, auth_service_1, app_injector_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -9,10 +9,32 @@ System.register(['angular2/platform/browser', './app.component'], function(expor
             },
             function (app_component_1_1) {
                 app_component_1 = app_component_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (auth_service_1_1) {
+                auth_service_1 = auth_service_1_1;
+            },
+            function (app_injector_1_1) {
+                app_injector_1 = app_injector_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(app_component_1.AppComponent, [])
-                .then(function (success) { return console.log("Bootstrap success"); })
+            browser_1.bootstrap(app_component_1.AppComponent, [
+                auth_service_1.AuthService,
+                http_1.HTTP_PROVIDERS,
+                router_1.ROUTER_PROVIDERS,
+                core_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy })
+            ]).then(function (appRef) {
+                // store a reference to the application injector
+                app_injector_1.appInjector(appRef.injector);
+            }).then(function (success) { return console.log("Bootstrap success"); })
                 .catch(function (error) { return console.log(error); });
         }
     }
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,19 @@
 import { bootstrap } from 'angular2/platform/browser';
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent, [])
-  .then(success => console.log(`Bootstrap success`))
-  .catch(error => console.log(error));
+import { HTTP_PROVIDERS } from 'angular2/http';
+import { provide, ComponentRef } from 'angular2/core';
+import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
+import { AuthService } from './auth/auth.service';
+import { appInjector } from './app-injector';
+
+bootstrap(AppComponent, [
+    AuthService,
+    HTTP_PROVIDERS, 
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, { useClass: HashLocationStrategy })
+]).then((appRef: ComponentRef) => {
+    // store a reference to the application injector
+    appInjector(appRef.injector);
+ }).then(success => console.log(`Bootstrap success`))
+   .catch(error => console.log(error));
