86d4e67adbf2fa35f04da73158b22cb07883f0c3
lekova <lina.lekova@gmail.com>
2016-06-01 16:05:29 -0400

    Add new material design to signup. Add validation.

--- app/auth/signup.component.js
+++ app/auth/signup.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '@angular/router-deprecated', './auth.service'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/common', '@angular/router-deprecated', './auth.service', '../blocks/utils/mdl', '../blocks/utils/emailValidator'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,58 +10,67 @@ System.register(['@angular/core', '@angular/router-deprecated', './auth.service'
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_deprecated_1, auth_service_1;
+    var core_1, common_1, router_deprecated_1, auth_service_1, mdl_1, emailValidator_1;
     var SignupComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
+            function (common_1_1) {
+                common_1 = common_1_1;
+            },
             function (router_deprecated_1_1) {
                 router_deprecated_1 = router_deprecated_1_1;
             },
             function (auth_service_1_1) {
                 auth_service_1 = auth_service_1_1;
+            },
+            function (mdl_1_1) {
+                mdl_1 = mdl_1_1;
+            },
+            function (emailValidator_1_1) {
+                emailValidator_1 = emailValidator_1_1;
             }],
         execute: function() {
             SignupComponent = (function () {
-                function SignupComponent(_router, _authService) {
+                function SignupComponent(_router, _authService, _formBuilder) {
                     this._router = _router;
                     this._authService = _authService;
+                    this._formBuilder = _formBuilder;
+                    this.submitAttempt = false;
+                    this.errorLabelShow = false;
+                    this.email = new common_1.Control('', common_1.Validators.compose([common_1.Validators.required, emailValidator_1.emailValidator]));
+                    this.password = new common_1.Control('', common_1.Validators.compose([common_1.Validators.required, common_1.Validators.minLength(6)]));
+                    this.confirmPassword = new common_1.Control('', common_1.Validators.required);
+                    this.signupForm = _formBuilder.group({
+                        'email': this.email,
+                        'password': this.password,
+                        'confirmPassword': this.confirmPassword
+                    });
                 }
-                SignupComponent.prototype.signup = function (event) {
+                SignupComponent.prototype.signup = function (user) {
                     var _this = this;
-                    event.preventDefault();
-                    // TODO
-                    // if this.userPassword != this.userConfirmPassword
-                    // display message "Passwords do not match"
-                    // and make them red
-                    this._authService.signup(this.userEmail, this.userPassword, this.userConfirmPassword).subscribe(function (result) {
-                        debugger;
+                    this.submitAttempt = true;
+                    if (!this.signupForm.valid)
+                        return;
+                    this._authService.signup(user.email, user.password, user.confirmPassword)
+                        .subscribe(function (result) {
                         if (result) {
                             _this._router.navigate(['Dashboard']);
                         }
+                    }, function (error) {
+                        _this.errorLabelShow = true;
                     });
                 };
-                __decorate([
-                    core_1.Input(), 
-                    __metadata('design:type', String)
-                ], SignupComponent.prototype, "userEmail", void 0);
-                __decorate([
-                    core_1.Input(), 
-                    __metadata('design:type', String)
-                ], SignupComponent.prototype, "userPassword", void 0);
-                __decorate([
-                    core_1.Input(), 
-                    __metadata('design:type', String)
-                ], SignupComponent.prototype, "userConfirmPassword", void 0);
                 SignupComponent = __decorate([
                     core_1.Component({
                         selector: 'signup',
                         templateUrl: 'app/auth/signup.component.html',
-                        styleUrls: ['app/auth/signup.component.css']
+                        styleUrls: ['app/auth/signup.component.css'],
+                        directives: [mdl_1.MDL, common_1.FORM_DIRECTIVES]
                     }), 
-                    __metadata('design:paramtypes', [router_deprecated_1.Router, auth_service_1.AuthService])
+                    __metadata('design:paramtypes', [router_deprecated_1.Router, auth_service_1.AuthService, common_1.FormBuilder])
                 ], SignupComponent);
                 return SignupComponent;
             }());
--- app/auth/signup.component.ts
+++ app/auth/signup.component.ts
@@ -1,36 +1,59 @@
 import { Component, Input } from '@angular/core';
+import { FORM_DIRECTIVES, Control, ControlGroup, FormBuilder, Validators } from '@angular/common';
 import { Router } from '@angular/router-deprecated';
 import { AuthService } from './auth.service';
+import { MDL } from '../blocks/utils/mdl';
+import { emailValidator } from '../blocks/utils/emailValidator';
+import { passwordValidator } from '../blocks/utils/passwordValidator';
 
 @Component({
     selector: 'signup',
     templateUrl: 'app/auth/signup.component.html',
-    styleUrls: ['app/auth/signup.component.css']
+    styleUrls: ['app/auth/signup.component.css'],
+    directives: [MDL, FORM_DIRECTIVES]
 })
 export class SignupComponent {
-    
-    @Input() userEmail: string;
-    @Input() userPassword: string;
-    @Input() userConfirmPassword: string;
-        
+
+    // @Input() userEmail: string;
+    // @Input() userPassword: string;
+    // @Input() userConfirmPassword: string;
+
+    private signupForm: ControlGroup;
+    private email: Control;
+    private password: Control;
+    private confirmPassword: Control;
+    private submitAttempt: boolean = false;
+    private errorLabelShow: boolean = false;
+
     constructor(
         private _router: Router,
-        private _authService: AuthService
-    ) { }
-    
-    signup(event: any) {
-        event.preventDefault();
+        private _authService: AuthService, 
+        private _formBuilder: FormBuilder
+    ) {
         
-        // TODO
-        // if this.userPassword != this.userConfirmPassword
-        // display message "Passwords do not match"
-        // and make them red
-        
-        this._authService.signup(this.userEmail, this.userPassword, this.userConfirmPassword).subscribe((result) => {
-            debugger;
-            if (result) {
-                this._router.navigate(['Dashboard']);
-            }
+        this.email = new Control('', Validators.compose([Validators.required, emailValidator]));
+        this.password = new Control('', Validators.compose([Validators.required, Validators.minLength(6)]));
+        this.confirmPassword = new Control('', Validators.required);
+
+        this.signupForm = _formBuilder.group({
+            'email': this.email,
+            'password': this.password,
+            'confirmPassword': this.confirmPassword
         });
+    }
+
+    signup(user: any) {
+        this.submitAttempt = true;
+        if (!this.signupForm.valid) return;
+        
+        this._authService.signup(user.email, user.password, user.confirmPassword)
+        .subscribe(
+            result => {
+                if (result) { this._router.navigate(['Dashboard']); }
+            }, 
+            error => {
+                this.errorLabelShow = true;
+                
+            });
     }
 }
